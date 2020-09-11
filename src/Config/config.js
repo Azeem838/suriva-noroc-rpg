49bdb536b161cf3866fc4129ab96b41a24826715
Azeem <azeemahmed101@gmail.com>
2020-09-11 21:44:25 +0200

    center canvas

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,10 +1,15 @@
 import Phaser from 'phaser';
 
+const DEFAULT_WIDTH = 800;
+const DEFAULT_HEIGHT = 600;
+
 export default {
   type: Phaser.AUTO,
-  parent: 'phaser-example',
-  width: 800,
-  height: 600,
+  parent: 'phaser-game',
+  mode: Phaser.Scale.FIT,
+  autoCenter: Phaser.Scale.CENTER_BOTH,
+  width: DEFAULT_WIDTH,
+  height: DEFAULT_HEIGHT,
   physics: {
     default: 'arcade',
     arcade: {
