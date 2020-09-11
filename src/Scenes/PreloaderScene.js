2fdf352d6de54ed5838c7ed81edbce788eae62ba
Azeem <azeemahmed101@gmail.com>
2020-09-11 09:53:20 +0200

    add preloader scene

---
+++ src/Scenes/PreloaderScene.js
@@ -0,0 +1,98 @@
+import 'phaser';
+
+export default class PreloaderScene extends Phaser.Scene {
+  constructor() {
+    super('Preloader');
+  }
+
+  init() {
+    this.readyCount = 0;
+  }
+
+  prelaod() {
+    this.add.image(400, 200, 'logo');
+
+    const progressBar = this.add.graphics();
+    const progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(240, 270, 320, 50);
+
+    const width = this.cameras.main.width;
+    const height = this.cameras.main.height;
+    const loadingText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 50,
+      text: 'Loading...',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff',
+      },
+    });
+    loadingText.setOrigin(0.5, 0.5);
+
+    const percentText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 5,
+      text: '0%',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff',
+      },
+    });
+    percentText.setOrigin(0.5, 0.5);
+
+    const assetText = this.make.text({
+      x: width / 2,
+      y: height / 2 + 50,
+      text: '',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff',
+      },
+    });
+    assetText.setOrigin(0.5, 0.5);
+
+    // update progress bar
+    this.load.on('progress', (value) => {
+      percentText.setText(`${parseInt(value * 100)} + %`);
+      progressBar.clear();
+      progressBar.fillStyle(0xffffff, 1);
+      progressBar.fillRect(250, 280, 300 * value, 30);
+    });
+
+    // update file progress text
+    this.load.on('fileprogress', function (file) {
+      assetText.setText('Loading asset: ' + file.key);
+    });
+
+    // remove progress bar when complete
+    this.load.on(
+      'complete',
+      function () {
+        progressBar.destroy();
+        progressBox.destroy();
+        loadingText.destroy();
+        percentText.destroy();
+        assetText.destroy();
+        this.ready();
+      }.bind(this),
+    );
+
+    this.timedEvent = this.time.delayedCall(3000, this.ready, [], this);
+
+    // load assets needed in our game
+    this.load.image('yellowButton', 'assets/ui/yellow-button.png');
+    this.load.image('leaderboard', 'assets/ui/leaderboard.png');
+    this.load.image('pause', 'assets/ui/pause.png');
+    this.load.image('play', 'assets/ui/play-now.png');
+    this.load.image('settings', 'assets/ui/settings.png');
+    this.load.audio('bgMusic', ['assets/audio/TownTheme.mp3']);
+  }
+
+  ready() {
+    this.scene.start('Title');
+    if (this.ready.count === 2) {
+      this.scene.start('Title');
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import 'phaser';
 import config from './Config/config';
 import BootScene from './Scenes/BootScene';
+import PreloaderScene from './Scenes/PreloaderScene';
 
 class Game extends Phaser.Game {
   constructor() {
     super(config);
     this.scene.add('Boot', BootScene);
+    this.scene.add('Preloader', PreloaderScene);
   }
 }
 
