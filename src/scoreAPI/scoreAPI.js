68560b5673093568ce25bc29695b7ebb68c2e107
Azeem <azeemahmed101@gmail.com>
2020-09-14 13:50:40 +0200

    add score API

---
+++ src/scoreAPI/scoreAPI.js
@@ -0,0 +1,34 @@
+const baseUrl =
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
+const gameId = 'lTnHA1Sj94RLGOrEcxLP';
+
+export const postScore = async (name, score) => {
+  const response = await fetch(`${baseUrl}/games/${gameId}/scores`, {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      Accept: 'Application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ user: name, score: Number(score) }),
+  });
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error('Something went wrong!');
+};
+
+export const getScores = async () => {
+  const response = await fetch(`${baseUrl}/games/${gameId}/scores`, {
+    method: 'Get',
+    mode: 'cors',
+    headers: {
+      Accept: 'Application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error('Something went wrong!');
+};
